3051e0778815698088f8e4febbe159ee0b55bce4
Verdoner <jx55oc@student.aau.dk>
2022-11-24 11:08:47 +0100

    Hello

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#define AGE_OF_JULIUS 10
 
 int main(void){
     printf("Hello World!");
+
+    for (int i = 0; i < AGE_OF_JULIUS; ++i) {
+        printf("MinkMette DumbTruck\n");
+
+    }
     return 0;
 }
