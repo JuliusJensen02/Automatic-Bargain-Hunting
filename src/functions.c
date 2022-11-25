69b2a5439f2584b813a28c33e70f94e6056723bb
CatAAU <YW65FQ@student.aau.dk>
2022-11-25 14:53:35 +0100

    Gruppe 2, kun lorte ændringer. Intet virker. Lortet crasher efter item nummer 3. Tobias foreslog p1 variable.

--- src/functions.c
+++ src/functions.c
@@ -17,7 +17,6 @@ int main()
     free(grocery_list);
 
     return 0;
-
 }
 
 void user_input ()
@@ -26,19 +25,21 @@ void user_input ()
     char **temp_grocery_list;
     int k = 0;
 
+    printf("Please write the items you want to find one by one:");
 
-    printf("Please write the items you want to find one by one>");
-
+    temp_grocery_list = malloc(sizeof(char*) * (number_of_items + 1));
     while (strcmp(item, "exit") != 0)
     {
-        temp_grocery_list = malloc(sizeof(char) * (number_of_items + 1));
+        int p1 = sizeof(char*) * (number_of_items + 1);
+        temp_grocery_list = realloc(temp_grocery_list,p1);
 
+        printf("%c", );
         scanf(" %s", item);
-        printf("item = %s\n",item);
+        printf("item = %s\n", item);
 
         if(strcmp(item, "exit") != 0)
         {
-            number_of_items += 1;
+            number_of_items = number_of_items + 1;
             printf("Number of items = %d\n",number_of_items);
 
 
@@ -46,19 +47,23 @@ void user_input ()
             strcpy(temp_grocery_list[k], item);
             //printf("Temp grocery list = %s\n",temp_grocery_list[k]);
 
-
-            print_grocery_list(temp_grocery_list);
             k++;
-            printf("If end of grocery list, please write 'exit'. If not write next item>");
+            print_grocery_list(temp_grocery_list);
+
+            printf("If end of grocery list, please write 'exit'. If not, write next item:");
         }
     }
+
     grocery_list = malloc(sizeof(char) * number_of_items);
 
     for (int i = 0; i < number_of_items; ++i)
     {
         strcpy(grocery_list[i],temp_grocery_list[i]);
     }
+
     free(temp_grocery_list);
+    //Mistanke: malloc fra temp_grocery_list ødelægger vores kode. Løsning: få free'et memory
+
 }
 
 void print_grocery_list(char **list)
