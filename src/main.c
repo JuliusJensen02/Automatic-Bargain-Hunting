83f5c73ad6471f8cca7a7a4133bafaa3ded6ed3d
Verdoner <jx55oc@student.aau.dk>
2022-12-05 21:22:16 +0100

    Peter: Headerfile fixing

--- src/main.c
+++ src/main.c
@@ -5,11 +5,11 @@
 
 items           available_items[NUMBER_OF_ITEMS]; // sets size of struct array
 items_data      item_data_stores_prices[NUMBER_OF_ITEMS*NUMBER_OF_STORES]; // sets size of struct array
+int number_of_list_items = 0;
+char **grocery_list;
 
-int main(void){
-
-    int number_of_list_items = 0;
-    char **grocery_list;
+int main(void)
+{
 
     loadData(available_items, item_data_stores_prices);
     user_input(grocery_list, number_of_list_items);
