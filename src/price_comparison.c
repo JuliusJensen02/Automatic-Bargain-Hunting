b9740218850b7080f79404247f6d8e1cd64fbcc7
JuliusJensen02 <113088719+JuliusJensen02@users.noreply.github.com>
2022-12-05 09:43:48 +0100

    Julius

---
+++ src/price_comparison.c
@@ -0,0 +1,23 @@
+#include "Data_collection.c"
+#include "functions.c"
+
+void compare_prices(void);
+
+int main(){
+    loadData();
+    user_input();
+    compare_prices();
+}
+
+void compare_prices(void){
+    for (int i = 0; i < number_of_items; ++i) {
+
+        for(int j = 0; j < NUMBER_OF_ITEMS*NUMBER_OF_STORES; ++j) {
+            if(strcmp(grocery_list[i], item_data[j].item_name) == 0){
+                printf("'%s' '%s'", item_data[j].item_name, item_data[j].item_store);
+            }
+        }
+
+    }
+
+}
