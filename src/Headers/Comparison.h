6fc24dc59a2f16f65ca79b081ccb8de5fb2f4a7d
Verdoner <jx55oc@student.aau.dk>
2022-12-06 11:23:53 +0100

    Peter: Prints total price for multiple stores now

--- src/Headers/Comparison.h
+++ src/Headers/Comparison.h
@@ -10,5 +10,6 @@ typedef struct{
 
 void compare_prices(char **grocery_list, int number_of_list_items, items_data *item_data);
 void printPrices_multiple_stores(int number_of_list_items);
+double total_multiple_store_price (int number_of_list_items);
 
 #endif //AUTOMATIC_BARGAIN_HUNTING_COMPARISON_H
