479049e9eb3d4a046ab86ac52ae6bddc991ed8cc
JuliusJensen02 <113088719+JuliusJensen02@users.noreply.github.com>
2023-01-18 15:45:30 +0100

    Julius: Readme

--- src/functions.c
+++ src/functions.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "Headers/Data_collection.h"
 #define MAX_ITEM_SIZE 4096
 #define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
@@ -10,7 +11,7 @@ void exit_failure (char **array);
 void assign_grocery_list (char **grocery_list, int number_of_list_items);
 int item_check (items *available_items, char item[MAX_ITEM_SIZE], char **temp, int number_of_list_items);
 int levenshtein(char *s1, char *s2);
-int minimum_number(int arr[], int size);
+int minimum_number(const int *arr);
 int already_exist_item(char item[MAX_ITEM_SIZE], char **temp, int number_of_list_items);
 
 char **temp_grocery_list;
@@ -29,36 +30,22 @@ void user_input (int *number_of_list_items, items *available_items)
     while (strcmp(item, "exit") != 0)
     {
         //fgets is the way the program accepts an input from the user
-        fgets(item,MAX_ITEM_SIZE, stdin);
-        item[strlen(item)- 1] = '\0'; //replaces the '\n' with '\0'
-
+        do{
+            fgets(item, MAX_ITEM_SIZE, stdin);
+            item[strlen(item) - 1] = '\0'; //replaces the '\n' with '\0'
+        }
+        while(item[0] == '\0');
         if(strcmp(item, "exit") != 0){
-            int valid_item = item_check(available_items,
-                                        item,
+            int valid_item = item_check(available_items, item,
                                         temp_grocery_list,
                                         *number_of_list_items);
-            if (valid_item == 0){
+            if(valid_item >= 0){
                 temp_grocery_list[*number_of_list_items] = malloc(strlen(item));
                 exit_failure(temp_grocery_list);
-
-                strcpy(temp_grocery_list[*number_of_list_items], item);
-
-                *number_of_list_items += 1;
-                printf("Number of items = %d\n",*number_of_list_items);
-
-                print_grocery_list(temp_grocery_list, *number_of_list_items);
-            }
-            else if(valid_item >= 1){
                 strcpy(temp_grocery_list[*number_of_list_items], available_items[valid_item].item_name);
-
                 *number_of_list_items += 1;
                 printf("Number of items = %d\n",*number_of_list_items);
-
                 print_grocery_list(temp_grocery_list, *number_of_list_items);
-                printf("%s does not exist, I have autocorrected it to '%s'.\n", item, available_items[valid_item].item_name);
-            }
-            else{
-                printf("This item is already in the grocery list.\n");
             }
             printf("If end of grocery list, please write 'exit'. If not, write next item:\n");
         }
@@ -96,20 +83,14 @@ void exit_failure (char **array)
  * @param grocery_list
  * @param number_of_list_items
  */
-void assign_grocery_list (char **grocery_list, int number_of_list_items)
-{
-
-    for (int i = 0; i < number_of_list_items; ++i)
-    {
+void assign_grocery_list (char **grocery_list, int number_of_list_items){
+    for (int i = 0; i < number_of_list_items; ++i){
         grocery_list[i] = malloc(MAX_ITEM_SIZE);
         exit_failure(grocery_list);
-
         strcpy(grocery_list[i],temp_grocery_list[i]);
     }
-
     printf("Final Grocery List:\n");
     print_grocery_list(grocery_list, number_of_list_items);
-
     free(temp_grocery_list);
 }
 
@@ -119,42 +100,58 @@ void assign_grocery_list (char **grocery_list, int number_of_list_items)
  * @param item the inputted item
  * @return
  */
-int item_check (items *available_items, char item[MAX_ITEM_SIZE], char **temp, int number_of_list_items)
-{
-    if(already_exist_item(item, temp, number_of_list_items)){
-     return -1;
-    }
+int item_check(items *available_items, char item[MAX_ITEM_SIZE], char **temp, int number_of_list_items){
     int distances[NUMBER_OF_ITEMS];
-    for (int i = 0; i < NUMBER_OF_ITEMS; ++i)
-    {
-        distances[i] = levenshtein(item, available_items[i].item_name);
-        if (distances[i] == 0)
-        {
-            return 0;
+    //Finds the distances between the input and all products
+    int i;
+    char *itemFiltered = item;
+    for (int j = 0; j < strlen(item); ++j) {
+        itemFiltered[j] = tolower(item[j]);
+    }
+    for(i = 0; i < NUMBER_OF_ITEMS; ++i){
+        char *product;
+        strcpy(product, available_items[i].item_name);
+        product[0] = tolower(product[0]);
+        distances[i] = levenshtein(item, product);
+        if(distances[i] == 0){
+            break;
         }
     }
-    int auto_correct_index = minimum_number(distances, NUMBER_OF_ITEMS);
+    int auto_correct_index = minimum_number(distances); //Finds index of the smallest distance
+    char firstCharProduct = tolower(available_items[auto_correct_index].item_name[0]);
     if(already_exist_item(available_items[auto_correct_index].item_name, temp, number_of_list_items)){
-        printf("%s does not exist, I have autocorrected it to '%s'.\n", item, available_items[auto_correct_index].item_name);
+        printf("This item is already in the grocery list.\n");
         return -1;
     }
-    return auto_correct_index;
+    if(itemFiltered[0] == firstCharProduct && distances[auto_correct_index] < 3){
+        printf("I could not find '%s', is it ok that I correct it to %s? Yes(y) No (n)\n", item, available_items[auto_correct_index].item_name);
+        char check;
+        scanf("%c", &check);
+        if(check == 'y' || check == 'Y'){
+            return auto_correct_index;
+        }
+        else{
+            return -1;
+        }
+    }
+    else{
+        printf("I couldn't find that item\n");
+        return -1;
+    }
 }
 
 int already_exist_item(char *item, char **temp, int number_of_list_items){
-    for (int i = 0; i < number_of_list_items; ++i)
-    {
-        if (strcmp(temp[i], item) == 0)
-        {
+    for (int i = 0; i < number_of_list_items; ++i){
+        if (strcmp(temp[i], item) == 0){
             return 1;
         }
     }
     return 0;
 }
 
-int minimum_number(int arr[], int size){
+int minimum_number(const int *arr){
     int index = 0, min = 1000;
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < NUMBER_OF_ITEMS; ++i) {
         if(arr[i] < min){
             index = i;
             min = arr[i];
